fd8472b29ef9407e6631c99c5e2471cedbd44726
Dan Pickett <dan.pickett@launchware.com>
2014-08-17 20:48:11 -0400

    wrap simple_form wrapper
    * every form should use this wrapper

--- app/helpers/preflight/application_helper.rb
+++ app/helpers/preflight/application_helper.rb
@@ -1,5 +1,9 @@
 module Preflight
   module ApplicationHelper
+    def preflight_form_for(record, options={}, &block)
+      simple_form_for(record, options.merge(wrapper: :foundation), &block)
+    end
+
     def flash_class_map
       {
         'notice' => 'success'
