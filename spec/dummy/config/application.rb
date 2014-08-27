bb209f8cb7d76b1b17c978ec649712ad588e9fa8
Dan Pickett <dan.pickett@launchware.com>
2014-08-27 18:10:33 -0400

    results from `rake app:rails:update`

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,19 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require "preflight"
 
 module Dummy
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
+require "preflight"
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
@@ -24,6 +20,8 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # For not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
-
